274f12d697c1086eb54f08d4f038430084ea1992
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-24 13:11:16 -0500

    Update BBB_GPIO.py

--- BBB_GPIO.py
+++ BBB_GPIO.py
@@ -1,1 +1,14 @@
 # BBB_GPIO
+
+
+
+class BBB_GPIO_PIN:
+  
+  def __init__(self,pinNum,direction):
+    if direction not in ['in', 'In', 'IN', 'out', 'Out', 'OUT']:
+      raise ValueError
+    self.pinNum = pinNum 
+    ifdirection in ['in', 'In', 'IN']:
+      self.direction = 'in'
+    else:
+      self.direction = 'out'
