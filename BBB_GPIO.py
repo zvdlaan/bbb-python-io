38e439b16d8925d8b1cddb52668a1bf242affe2b
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-24 13:42:26 -0500

    Update BBB_GPIO.py

--- BBB_GPIO.py
+++ BBB_GPIO.py
@@ -1,14 +1,27 @@
 # BBB_GPIO
 
 
+def RunCommand(self, command ):
+	process = subprocess.Popen( command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True )	
+	response = process.communicate()
+	returnDict = { 'returncode': process.returncode, 'output':response[0].rstrip(), 'error': response[1].rstrip() }
+	return returnDict
 
-class GPIO_PIN:
+class GPIO_PIN_BASE:
   
   def __init__(self,pinNum,direction):
     if direction not in ['in', 'In', 'IN', 'out', 'Out', 'OUT']:
-      raise ValueError("""GPIO_PIN( pinNum, direction) couldn't be constructed b/c invalid direction.  Valid options: 'in', 'out'""")
+      raise ValueError("""direction must be 'in' or 'out'""")
     self.pinNum = pinNum 
     if direction in ['in', 'In', 'IN']:
       self.direction = 'in'
     else:
       self.direction = 'out'
+      
+  def ExportToGpioLib(pinNum):
+    command = """sudo sh -c "echo '""" + pinNum + """' > /sys/class/gpio/export" """ 
+    export = RunCommand( command )
+    print("returncode: " + str(export['returncode']))
+    print("output: " + export['output'])
+    print("error: " + export['error'])
+    
