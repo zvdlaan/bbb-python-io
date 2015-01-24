0e8c191f0916670ff8ea63a7abef5e409af1f7b0
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-24 13:20:38 -0500

    Update BBB_GPIO.py

--- BBB_GPIO.py
+++ BBB_GPIO.py
@@ -6,7 +6,7 @@ class GPIO_PIN:
   
   def __init__(self,pinNum,direction):
     if direction not in ['in', 'In', 'IN', 'out', 'Out', 'OUT']:
-      raise ValueError
+      raise ValueError("""GPIO_PIN( pinNum, direction) couldn't be constructed b/c invalid direction.  Valid options: 'in', 'out'""")
     self.pinNum = pinNum 
     if direction in ['in', 'In', 'IN']:
       self.direction = 'in'
